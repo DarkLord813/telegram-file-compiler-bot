04de8e585d470d4545cdfb9eb922ce81ad4fb897
DarkLord813 <rexoronsaye@gmail.com>
2025-10-25 10:11:01 +0100

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Bot Token from @BotFather
-BOT_TOKEN = os.getenv('BOT_TOKEN', "7870275570:AAF-gKYC3N7W586IBBFpe3gBmiF_xsgLVwQ")
+BOT_TOKEN = os.getenv('BOT_TOKEN', "your_bot_token_here")
 
 # Optional: Admin user IDs (for special features)
 ADMIN_IDS = [7475473197, 7713987088]
